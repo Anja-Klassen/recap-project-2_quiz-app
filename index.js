81165f16303a11ae051d1a6e4b9819c9d12551e2
Anja-Klassen <anja.klassen@posteo.de>
2024-11-01 10:53:11 +0100

    add first steps bookmark button toggle

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // phew… not a lot going on here. Please add some code!
+// von der card. und nicht vom document. auswählen
+const firstCard = document.querySelector("[data-js='first_card']");
+const bookmarkButton = firstCard.querySelector("[data-js='bookmark_button']");
+bookmarkButton.addEventListener("click", () => {
+    bookmarkButton.classList.toggle("bookmark--active");
+});
