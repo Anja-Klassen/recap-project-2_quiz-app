0470ef04e386952ea7b33dd896746ed51c18a485
Anja-Klassen <anja.klassen@posteo.de>
2025-01-16 14:01:05 +0100

    add solution exc 3

--- form.js
+++ form.js
@@ -1,19 +1,42 @@
 const form = document.querySelector('[data-js="form"]');
-const cardContainer = document.querySelector('[data-js=')
-const questionInput = document.querySelector('[data-js="question-input"]');
-const answerInput = document.querySelector('[data-js="answer-input"]');
-
+const cardContainer = document.querySelector("[data-js=card-container]");
 
 form.addEventListener("submit", (event) => {
     event.preventDefault();
 
     const formElement = event.target;
-    const formData = new FormData(event.target);
+    const formData = new FormData(formElement);
     const data = Object.fromEntries(formData);
     
-    const card = document.createElement("article");
-    card.classList.add("card");
+    // neue Karten Liste erstellen
+    const cardElement = document.createElement("li");
+    cardElement.classList.add("card-list__item");
+    cardContainer.append(cardElement);
+ 
+    // neue Karte erstellen
+    const newCard = document.createElement("article");
+    newCard.classList.add("card");
+    cardElement.append(newCard);
 
+    // Frage hinzufügen
     const questionElement = document.createElement("h2");
-    questionElement.textContent = data.questionInput
+    questionElement.textContent = data.question;
+    questionElement.classList.add("card__question");
+    newCard.append(questionElement);
+
+    // Antwort hinzufügen
+    const answerElement = document.createElement("p");
+    answerElement.textContent = data.answer;
+    answerElement.classList.add("card__answer--active");
+    newCard.append(answerElement);
+
+    //Tag Container hinzufügen
+    const tagList = document.createElement("ul");
+    tagList.classList.add("card__tag-list");
+    newCard.append(tagList);
+
+    const tagElement = document.createElement("li");
+    tagElement.textContent = data.tag;
+    tagElement.classList.add("card__tag-list-item");
+    tagList.append(tagElement);
 })
